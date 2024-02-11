d2f113ca9c0cc795808e24fd05cca96e06c265af
lucianacostilla <costilalu@gmail.com>
2024-02-11 17:56:25 -0300

    problemas con la key 2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "webserver" {
   ami                         = "ami-05fb0b8c1424f266b"
   instance_type               = "t2.small"
-  key_name                    = "/home/taemnilu/Desktop/pin3" 
+  key_name                    = "/pin3/mykey" 
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
